3f1e31ae679e9298919bd8c974b1b1f1ea2b5a05
MaratLoreev <125279653+MaratLoreev@users.noreply.github.com>
2023-12-05 11:36:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+
+
+st.title('Привет! Это мой первый сайн на Streamlit')
